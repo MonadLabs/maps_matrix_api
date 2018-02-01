656e7f1a98664d6d21fa06444f281de5f6cc2fbf
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2018-02-01 15:03:36 -0600

    First Commit

---
+++ lib/maps_matrix_api.ex
@@ -0,0 +1,18 @@
+defmodule MapsMatrixApi do
+  @moduledoc """
+  Documentation for MapsMatrixApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapsMatrixApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/maps_matrix_api_test.exs
@@ -0,0 +1,8 @@
+defmodule MapsMatrixApiTest do
+  use ExUnit.Case
+  doctest MapsMatrixApi
+
+  test "greets the world" do
+    assert MapsMatrixApi.hello() == :world
+  end
+end
